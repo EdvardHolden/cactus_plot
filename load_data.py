43b5c1bf937fc66a2da43c25e2d7a9d36e0b2e0d
EdvardHolden <edvardholden@gmail.com>
2022-11-04 21:46:22 +0100

    Implement new json data loader

---
+++ load_data.py
@@ -0,0 +1,81 @@
+import json
+from typing import List, Dict
+import numpy as np
+
+
+class Program:
+    def __init__(self, program_name: str, program_alias: str, positive_instance_stats: List[float]):
+        self.name = program_name
+        self.alias = program_alias
+        self.stats = positive_instance_stats
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_alias(self) -> str:
+        return self.alias
+
+    def get_max_val(self) -> float:
+        return max(self.stats)
+
+    def get_min_val(self) -> float:
+        return min(self.stats)
+
+    def get_average_val(self) -> float:
+        return np.average(self.stats)
+
+    def get_values(self) -> List[float]:
+        return self.stats
+
+    def __len__(self):
+        return len(self.stats)
+
+    def __str__(self):
+        return f"program_{self.get_name()}_solved_{self.__len__()}"
+
+
+def load_json_data_from_file(file_path, stat_type, max_val, min_val) -> Program:
+    with open(file_path, "rb") as f:
+        data = json.load(f)
+
+    # Get name attributes
+    name = data["preamble"]["program"]
+    alias = data["preamble"]["prog_alias"]
+
+    solved_stat = []
+    for _, entry in data["stats"].items():
+        if entry["status"]:
+            # Get the value and check the requirements
+            value = entry[stat_type]
+            if min_val <= value <= max_val:
+                solved_stat.append(value)
+
+    return Program(name, alias, solved_stat)
+
+
+def load_data(file_paths, args) -> Dict[str, Program]:
+
+    # Compute the min value
+    if args["plot_type"] == "scatter" and args['x_min']:
+        min_val = max(args["x_min"], args["y_min"])
+    else:
+        min_val = args["y_min"]  # options['y_min'] is always defined
+
+    # Load the data into a list of program objects
+    data = []
+    if args["data_type"] == "json":
+        for file_path in file_paths:
+            file_data = load_json_data_from_file(file_path, args["stat_type"], args["timeout"], min_val)
+            data += [file_data]
+    else:
+        raise ValueError(f"Unknown data type \"{args['data_type']}\"")
+
+    # Return Program attempt objects as a dict
+    return {p.get_alias(): p for p in data}
+
+
+if __name__ == "__main__":
+    # Testing function
+    d = load_json_data_from_file("examples/solver2.json", "rtime", 500, 0)
+    print(d)
+    print(type(d))
